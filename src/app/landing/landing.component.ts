c76d9e795617e0eaa60e53f6d3ae02004344baf8
Shady Mohamed <Shady.Mohamed@URANIUM-CORP.COM>
2023-11-13 01:00:41 +0200

    most popular function is added

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ExServices } from '../ex-service.service';
+import { CurrencyNames } from './models/currencyNames.model';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.sass']
+  styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
   value: number = 0;
-  fromValue?: string ;
-  toValue?: string ;
-  ratesDate? : any;
-  rateNames? : string[];
-  result?:number | string;
-  base: number = 0; 
+  fromValue?: string;
+  toValue?: string;
+  ratesDate?: any;
+  rateNames?: string[];
+  result?: number | string;
+  base: number = 0;
+  mostPopular: Array<CurrencyNames> = [
+    { code: 'USD', name: 'United States Dollar' },
+    { code: 'EUR', name: 'Euro' },
+    { code: 'GBP', name: 'Pound sterling' },
+    { code: 'CAD', name: "Canadian Dollar" },
+    { code: 'AUD', name: "Australian Dollar" },
+    { code: 'CHF', name: "Swiss Franc" },
+    { code: 'NZD', name: "New Zealand Dollar" },
+    { code: 'CNY', name: "Chinese Yuan" },
+    { code: 'HKD', name: "Hong Kong Dollar" }
+
+  ]
 
 
   constructor(private currencyService: ExServices) {
@@ -20,31 +33,43 @@ export class LandingComponent implements OnInit {
   }
   ngOnInit() {
     this.currencyService.getRates()
-      .subscribe(r =>  {
-        
+      .subscribe(r => {
+
         this.ratesDate = r.rates;
         this.rateNames = Object.keys(r.rates);
         this.fromValue = "EUR";
         this.toValue = "USD";
         this.base = this.ratesDate["EUR"];
-
+        this.getMostPopular();
       }
-        
-        )
+
+      )
   }
 
   convert = () => {
     let from = this.base / this.ratesDate[`${this.fromValue}`];
     let to = this.base / this.ratesDate[`${this.toValue}`]
-    this.result = this.value * from / to;  
-
+    this.result = this.value * from / to;
+    this.getMostPopular();
   }
-  swap = () =>{
+  swap = () => {
     let oldFrom = this.fromValue;
     let oldTo = this.toValue;
     this.fromValue = oldTo;
     this.toValue = oldFrom;
-    this.value ? this.convert() : "";
+    this.value ? this.convert() : this.result = "";
+    this.getMostPopular();
+  }
+  getMostPopular = (resetResult?:boolean) => {
+    this.mostPopular = this.mostPopular.map(c => {
+      c.rate = this.ratesDate[c.code];
+      c.equals = (this.base / (this.base / this.ratesDate[`${this.fromValue}`])) / (this.base / this.ratesDate[c.code])
+      return c
+    })
+    resetResult ? this.result = '':''
+    // console.log(rates)
+
+
   }
 }
 
---
+++ src/app/landing/models/currencyNames.model.ts
@@ -0,0 +1,6 @@
+export interface CurrencyNames {
+    code: string;
+    name: string;
+    rate?: number;
+    equals?: number;
+}
